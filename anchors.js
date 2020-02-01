0ac17cea061a4921ce22c2fc6ae8ba7b7f4472ae
Stanislav Shinkovich <stanislavshinkovich@gmail.com>
2020-02-01 11:24:11 +0200

    fixed bug with mobile version

--- anchors.js
+++ anchors.js
@@ -6,16 +6,18 @@ for (let anchor of anchors) {
 
         const blockID = anchor.getAttribute('href').substr(1);
 
+        headerMenu.classList.remove("toggled");
+        burger.classList.remove("burger-wrapper-on");
 
 
         document.getElementById(blockID).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
-        document.getElementById(blockID).classList.toggle("red");
-        setTimeout(       ()=>  document.getElementById(blockID).classList.toggle("red")
-    ,1500
-    );
+    //     document.getElementById(blockID).classList.toggle("red");
+    //     setTimeout(       ()=>  document.getElementById(blockID).classList.toggle("red")
+    // ,1500
+    // );
 
     })
 }
